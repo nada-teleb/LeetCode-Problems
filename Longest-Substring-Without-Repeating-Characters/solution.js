e612081b00081b8a89c3da3a6a5cc31aa57eb250
nada_teleb <telebnada@gmail.com>
2024-05-26 15:57:28 +0300

    Adding another 'Longest Substring Without Repeating Characters' problem solution file, used an array to save all possible substrings to loop through for longest one, but even worse runtime and memory consumption.

--- Longest-Substring-Without-Repeating-Characters/solution.js
+++ Longest-Substring-Without-Repeating-Characters/solution.js
@@ -7,27 +7,21 @@ var lengthOfLongestSubstring = function (s) {
 	if (s.length == 1)
 		return 1;
 
-	var longestString = '';
-	var tempLongestString = '';
+	var possibleSubStrings = [''];
 
 	var secondIndex = 0;
 
 	for (var index = secondIndex; index < s.length; index++) {
 
-		if (!tempLongestString.includes(s[index])) {
-			tempLongestString += s[index];
+		if (!possibleSubStrings[secondIndex].includes(s[index])) {
+
+			possibleSubStrings[secondIndex] += s[index];
 		}
 		else {
-			if (longestString.length < tempLongestString.length)
-				longestString = tempLongestString;
-			secondIndex++;
-			index = secondIndex;
-			tempLongestString = s[secondIndex];
+			index = ++secondIndex;
+			possibleSubStrings[secondIndex] = s[secondIndex];
 		}
 	}
 
-	if (longestString.length < tempLongestString.length)
-		longestString = tempLongestString;
-
-	return longestString.length;
+	return Math.max(...(possibleSubStrings.map(el => el.length)));
 };
